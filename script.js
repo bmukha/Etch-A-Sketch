fd330b879c8e05e13cb7891a9f84d59557136ddb
bmukha <47007157+bmukha@users.noreply.github.com>
2020-10-03 17:22:21 +0300

    reset works correctly

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const mainContainer = document.querySelector("#main-container");
-for (let i = 0; i < 256; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.textContent = i + 1;
-  mainContainer.appendChild(newDiv);
+
+function fillTheGrid(number = 16) {
+  mainContainer.innerHTML = "";
+  mainContainer.style.cssText = `grid-template-columns: repeat(${number}, 1fr);
+      grid-template-rows: repeat(${number}, 1fr);`;
+  let numSquare = number * number;
+  for (let i = 0; i < numSquare; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.addEventListener(
+      "mouseleave",
+      () => (newDiv.style.backgroundColor = "green")
+    );
+    mainContainer.appendChild(newDiv);
+  }
 }
-console.log(mainContainer);
+
+fillTheGrid();
+
+const resetBtn = document.querySelector("#btn-reset");
+resetBtn.addEventListener("click", () => {
+  let newSize = "";
+  do {
+    newSize = prompt("Enter new size between 1 and 100", "");
+  } while (+newSize < 1 || newSize > 100);
+  fillTheGrid(newSize);
+});
